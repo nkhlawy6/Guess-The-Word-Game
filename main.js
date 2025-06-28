0ddc07f4bc70643fae1a134d9958d95a486688a2
emauser1 <emauser1@mci.ext>
2025-06-28 07:34:29 -0700

    some changes styling

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-let arr = ["🍎", "🍌", "🍓", "🍇"];
-let count=0;
-
-document.getElementById('current').textContent=arr[count];
-
-function guess(userGuess){
-  count++;
-  if(count>=arr.length){
-    count=0;
-  }
-let next=arr[count]
- if(userGuess===next){
-  document.getElementById('result').innerHTML+='✅ Well done!<br>';
- }else{
-  document.getElementById('result').textContent='Nice try! But the correct answer is...'+next;
- }
-
- document.getElementById('current').textContent=next;
-}
+let gameName='Guess The Word';
+document.title=gameName;
+document.getElementById('gameTitle').innerHTML=gameName;
+document.querySelector('.footer').innerHTML=`${gameName} Game Created By MEl-Nakhlawy`
