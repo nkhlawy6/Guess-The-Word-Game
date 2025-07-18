5a580e8ea7d7cfba34f8efc5178818eecea538a4
emauser1 <emauser1@mci.ext>
2025-07-18 10:41:05 +0300

    generate responses based on user messages using free gemini API

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -4,12 +4,41 @@ const sendMessageBtn = document.querySelector("#send-message");
 const userData = {
   message: null,
 };
-
+const API_KEY = "AIzaSyBex6ES91h8cfhrkqZXoD-DFyDQ4NNyFQU";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 const createMessageElement = (content, ...classes) => {
   const div = document.createElement("div");
   div.classList.add("message", ...classes);
   div.innerHTML = content;
   return div;
+};
+
+const generateBotRespnse = async (ingoingMessageDiv) => {
+  const messageElement=ingoingMessageDiv.querySelector('.message-text');
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      contents: [{ parts: [{ text: userData.message }] }],
+    }),
+  };
+  try {
+    const response = await fetch(API_URL, requestOptions);
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error.message);
+    console.log(data);
+    const apiResponeText=data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim();
+    messageElement.innerHTML=apiResponeText
+  } catch (error) {
+    console.log(error);
+    messageElement.innerHTML=error.message;
+    messageElement.style.color='#ff0000'
+
+  }finally{
+    ingoingMessageDiv.classList.remove('thinking')
+  chatBody.scrollBy({top:chatBody.scrollHeight,behavior:'smooth'})
+
+  }
 };
 function handleOutgoingMessage(e) {
   e.preventDefault();
@@ -23,6 +52,7 @@ function handleOutgoingMessage(e) {
   outgoingMessageDiv.querySelector(".message-text").textContent =
     userData.message;
   chatBody.appendChild(outgoingMessageDiv);
+  chatBody.scrollBy({top:chatBody.scrollHeight,behavior:'smooth'})
   setTimeout(() => {
     const messageContent = `
      <img class="chat-logo" src="assets/svg/icon.svg" alt="" />
@@ -42,6 +72,8 @@ function handleOutgoingMessage(e) {
     outgoingMessageDiv.querySelector(".message-text").textContent =
       userData.message;
     chatBody.appendChild(ingoingMessageDiv);
+  chatBody.scrollBy({top:chatBody.scrollHeight,behavior:'smooth'})
+    generateBotRespnse(ingoingMessageDiv);
   }, 600);
 }
 
@@ -53,3 +85,5 @@ botMessageAreaa.addEventListener("keydown", (e) => {
 });
 
 sendMessageBtn.addEventListener("click", (e) => handleOutgoingMessage(e));
+
+//generate  responses  based on user messages using the free gemini API
